e591063ded4c40ac8a7efbbb7744d0fea5241964
Youhoseong <h0song@naver.com>
2021-05-17 19:17:20 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+
+      },
+
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+      width: {
+        "31/64": "48.4375%",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
